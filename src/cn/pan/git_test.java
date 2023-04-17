bf572f938df35d28130dd59b51b72f93352a1804
gobeginner <102808418+gobeginner@users.noreply.github.com>
2023-04-17 20:55:52 +0800

    Update git_test.java
    github中修改的代码

--- src/cn/pan/git_test.java
+++ src/cn/pan/git_test.java
@@ -2,6 +2,6 @@ package cn.pan;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("helloworld666");
     }
 }
